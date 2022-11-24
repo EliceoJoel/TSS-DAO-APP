94ffcfa999969fd2f3fc59240b1a4e23ca5e83ea
Eliceo Joel <eliceoherbas@gmail.com>
2022-11-23 22:04:29 -0400

    TSS-4-1: Show alert dialog when user confirm their location and is out of range

--- app/src/main/java/com/tssdao/mytssapp/SelectDestinyActivity.java
+++ app/src/main/java/com/tssdao/mytssapp/SelectDestinyActivity.java
@@ -15,11 +15,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.tssdao.mytssapp.databinding.ActivitySelectDestinyBinding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class SelectDestinyActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivitySelectDestinyBinding binding;
     private Button btnConfirmDestinty;
+    private List<Double> distancesBetweenAgencyAndCurrentLocation;
+
+    //The maximum distance between the client and agencies in kilometers to take into account before send cars to the client
+    public static double MAX_DISTANCE_BETWEEN_AGENCY_AND_CURRENT_LOCATION = 5.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,11 @@ public class SelectDestinyActivity extends FragmentActivity implements OnMapRead
 
         binding = ActivitySelectDestinyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //Initialize list of distances between agencies and current location
+        //Esto solo es un ejemplo de las posibles distancias entre el usuario y las agencias al momento de solicitar el servicio
+        //Para que pase al otro activity reducir alguna distancia entre 0.0 y 5.0
+        distancesBetweenAgencyAndCurrentLocation = Arrays.asList(5.3, 7.3, 6.3, 5.3);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -38,10 +54,27 @@ public class SelectDestinyActivity extends FragmentActivity implements OnMapRead
         btnConfirmDestinty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SelectDestinyActivity.this, TravelInformationActivity.class);
-                startActivity(intent);
+                if(!isOutOfRange()) {
+                    Intent intent = new Intent(SelectDestinyActivity.this, TravelInformationActivity.class);
+                    startActivity(intent);
+                } else {
+                    showOutOfRangeDialog();
+                }
             }
         });
+    }
+
+    private boolean isOutOfRange() {
+        double minDistance = Collections.min(distancesBetweenAgencyAndCurrentLocation);
+        return minDistance <= MAX_DISTANCE_BETWEEN_AGENCY_AND_CURRENT_LOCATION ? false : true;
+    }
+
+    private void showOutOfRangeDialog() {
+        SimpleAlertDialog alertDialog = new SimpleAlertDialog(
+                "Fuera de rango",
+                "La distancia desde donde se solicita el servicio esta muy alejada de nuestras agencias, mantente dentro de los 5km",
+                "OK");
+        alertDialog.show(getSupportFragmentManager(), "Alert dialog");
     }
 
     /**
